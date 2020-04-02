cc55f30e92725d2f669dfea6e4c02fc6c2c8519b
涂芳 <928983454@qq.com>
2020-04-02 09:53:21 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+// 完整引入
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// 按需引入
+import './plugin/element.js'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
